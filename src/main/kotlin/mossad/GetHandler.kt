c6ecd366142fd227ed6699f270cac9be118239bf
Ricardo Baumann <gemini.richard@gmail.com>
2017-12-19 14:35:18 +0100

    working lambda support

--- src/main/kotlin/mossad/GetHandler.kt
+++ src/main/kotlin/mossad/GetHandler.kt
@@ -5,12 +5,16 @@ import com.amazonaws.services.lambda.runtime.RequestStreamHandler
 import com.github.kittinunf.fuel.core.requests.write
 import java.io.InputStream
 import java.io.OutputStream
+import java.util.*
+
+data class ApiGatewayResponse(val body: String, val headers: Map<String, String> = Collections.singletonMap("Content-Type", "application/json"), val statusCode: Int = 200, val isBase64Encoded: Boolean = false)
 
 class GetHandler : RequestStreamHandler {
     override fun handleRequest(input: InputStream?, output: OutputStream?, context: Context?) {
-        jedis["test"] = "test"
-        val result = jedis["test"]
-        output.write("Im running $result")
+        val inputJson = objectMapper.readTree(input)
+        val id = inputJson["pathParameters"]["id"].asText()
+        val result = jedis[id] ?: "[]"
+        output.write(objectMapper.writeValueAsString(ApiGatewayResponse(body = result)))
     }
 
 }
