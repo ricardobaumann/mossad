9f615b31592dd3948ed69811b812f592e41706d9
Ricardo Baumann <gemini.richard@gmail.com>
2017-12-20 15:19:48 +0100

    Infering redis endpoint from serverless info

--- src/main/kotlin/mossad/Config.kt
+++ src/main/kotlin/mossad/Config.kt
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import redis.clients.jedis.Jedis
 import java.util.*
 
-//TODO infer database url on serverless
-val jedis by lazy { Jedis("mos-se-1o51np5j4txno.r2wjf6.0001.euw1.cache.amazonaws.com", 6379) }
+val jedis by lazy { Jedis(System.getenv("redisEndpoint") ?: "localhost", (System.getenv("redisPort") ?: "6379").toInt()) }
 
 val objectMapper = ObjectMapper()
 
