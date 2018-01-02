0e78f6ded44f748474f868fd025e41507d486a84
Ricardo Baumann <gemini.richard@gmail.com>
2018-01-02 14:00:04 +0100

    Id device index support

--- src/main/kotlin/mossad/Extractor.kt
+++ src/main/kotlin/mossad/Extractor.kt
@@ -4,25 +4,18 @@ import com.fasterxml.jackson.databind.node.ArrayNode
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.jackson.responseObject
 import com.github.kittinunf.result.Result
-import java.time.LocalDateTime
-import java.util.*
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.function.Supplier
-import java.util.stream.Collectors
-import java.util.stream.IntStream
-import java.util.stream.Stream
-import kotlin.system.exitProcess
+import kotlin.collections.HashMap
 
-private val pageSize = (System.getenv("pageSize") ?: "500").toInt()
+enum class IndexMode {
+    ID, ID_AND_DEVICE
+}
 
-private val maxPages = (System.getenv("maxPages") ?: "10").toInt()
 
-private val visitorsLogPiwikUrl = "$piwikBaseUrl?module=API&method=Live.getLastVisitsDetails&format=JSON" +
-        "&idSite=1&period=day&date=today&expanded=1&filter_sort_column=lastActionTimestamp&filter_sort_order=desc" +
-        "&showColumns=actionDetails&token_auth=${params.piwikToken}&filter_limit=$pageSize"
-
-private fun String.extractContentId(): String {
+private fun String.asArticleId(): String {
     val parts = this.split("/")
     if (parts.size < 2) {
         return this
@@ -33,19 +26,50 @@ private fun String.extractContentId(): String {
 
 fun main(args: Array<String>) {
     val start = System.currentTimeMillis()
-    extractRecommendations()
+    val result = extractRecommendations()
+    result.entries.forEach { println("${it.key} = ${it.value}") }
     println("Finished with ${System.currentTimeMillis()-start} millis")
 }
 
-fun extractRecommendations(): Map<String, MutableList<String>> {
+fun extractRecommendations(indexMode: IndexMode = IndexMode.ID): Map<String, List<String>> {
+    val pageSize = (System.getenv("pageSize") ?: "500").toInt()
+    val maxPages = (System.getenv("maxPages") ?: "2").toInt()
+    val visitorsLogPiwikUrl = "$piwikBaseUrl?module=API&method=Live.getLastVisitsDetails&format=JSON" +
+            "&idSite=1&period=day&date=today&expanded=1&filter_sort_column=lastActionTimestamp&filter_sort_order=desc" +
+            "&showColumns=actionDetails,deviceType&token_auth=${params.piwikToken}&filter_limit=$pageSize"
+
     val threadPool = Executors.newFixedThreadPool((System.getenv("threadPoolSize") ?: "10").toInt())
+    val futures = buildPiwikFutureCalls(maxPages, threadPool, visitorsLogPiwikUrl, 10)
+
+    CompletableFuture.allOf(*futures.toTypedArray()).get()
+    threadPool.shutdown()
+    val results = futures.map { it.join() }
+    val map = if (indexMode == IndexMode.ID) results.extractByIdRelations() else results.extractByIdAndDeviceRelations()
+    return map.entries.associate { it.key to extractMostHit(it.value) }
+
+}
+
+private fun List<ArrayNode>.extractByIdAndDeviceRelations(): HashMap<String, MutableList<String>> {
+    val fromToIds = HashMap<String, MutableList<String>>()
+    this.flatMap { it }.map {
+        Pair<String,List<String>>(it["deviceType"].asText(), (it["actionDetails"] as ArrayNode)
+                .map { it["url"].asText().asArticleId() }
+                .filter { it.isNumeric() }) }
+            .filter { it.second.size > 1 }
+            .forEach { it.second.forEach { item -> fromToIds.getOrPut("$item-${it.first}",{ mutableListOf() }).addAll(it.second.filter { id -> id!=item }) } }
+
+    return fromToIds
+}
+
+private fun buildPiwikFutureCalls(maxPages: Int, threadPool: ExecutorService?, piwikUrl: String, pageSize: Int): List<CompletableFuture<ArrayNode>> {
     val emptyResponse = objectMapper.createArrayNode()
-    val futures = IntStream.rangeClosed(0, maxPages).mapToObj { page ->
+
+    return (0..maxPages).map { page ->
         CompletableFuture.supplyAsync(Supplier {
             try {
                 println("Starting page $page")
                 val offset = page * pageSize
-                val (_, _, result) = ("$visitorsLogPiwikUrl&filter_offset=$offset")
+                val (_, _, result) = ("$piwikUrl&filter_offset=$offset")
                         .httpGet().timeout(60000).timeoutRead(60000).responseObject<ArrayNode>()
                 when (result) {
                     is Result.Failure -> {
@@ -64,31 +88,25 @@ fun extractRecommendations(): Map<String, MutableList<String>> {
 
         }, threadPool)
 
-    }.collect(Collectors.toList())
-
-    CompletableFuture.allOf(*futures.toTypedArray()).get()
-    threadPool.shutdown()
-    return futures.stream().map { it.join() }.extractRelations().entries.stream().collect(Collectors.toList()).associate { it.key to extractMostHit(it.value) }
-
+    }
 }
 
-private fun Stream<ArrayNode>.extractRelations(): HashMap<String, MutableList<String>> {
+private fun List<ArrayNode>.extractByIdRelations(): HashMap<String, MutableList<String>> {
     val fromToIds = HashMap<String, MutableList<String>>()
-    this.flatMap { it.stream() }.map { visit -> visit["actionDetails"] as ArrayNode }
+    this.flatMap { it }.map { visit -> visit["actionDetails"] as ArrayNode }
             .map { actionDetails ->
-                actionDetails.stream()
+                actionDetails
                         .map { actionDetail -> actionDetail["url"].asText() }
-                        .collect(Collectors.toList())
             }
             .forEach { urls ->
-                val ids = urls.stream()
-                        .map { it.extractContentId() }
-                        .filter { it.isNumeric() }.collect(Collectors.toList())
+                val ids = urls
+                        .map { it.asArticleId() }
+                        .filter { it.isNumeric() }
 
                 ids.forEach { key ->
-                    val value = ids.stream().filter { it != key }.collect(Collectors.toList())
+                    val value = ids.filter { it != key }
                     if (value.isNotEmpty()) {
-                        fromToIds.getOrPut(key, { value }).addAll(value)
+                        fromToIds.getOrPut(key, { mutableListOf() }).addAll(value)
                     }
                 }
 
@@ -97,14 +115,10 @@ private fun Stream<ArrayNode>.extractRelations(): HashMap<String, MutableList<St
     return fromToIds
 }
 
-fun extractMostHit(inputList: MutableList<String>): MutableList<String> {
-
-    return inputList.stream().collect(Collectors.groupingBy({ w -> w }, Collectors.counting()))
-            .entries.stream()
-            .sorted(Comparator.comparingInt<MutableMap.MutableEntry<Any?, Long>> { value -> value.value.toInt() }
-                    .reversed())
-            .limit(10).map { it.key }
-            .map { it.toString() }
-            .collect(Collectors.toList())
+fun extractMostHit(inputList: List<String>): List<String> {
+    return inputList.groupingBy { it }.eachCount()
+            .entries.sortedBy { it.value }.reversed()
+            .take(10)
+            .map { it.key }
 
 }
